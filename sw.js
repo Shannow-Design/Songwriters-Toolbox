3682181278bc5e439b9520530e38b20b7d38ce15
Shannow-Design <stuart@shannow-design.com>
2025-12-19 09:22:35 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'musician-toolbox-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './manifest.json',
+  './icon.png',
+  './modules/app.js',
+  './modules/audio.js',
+  './modules/chords.js',
+  './modules/fretboard.js',
+  './modules/theory.js'
+];
+
+// Install Event: Cache files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching app assets...');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Fetch Event: Serve from Cache if available
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // If found in cache, return it. If not, go to network.
+      return response || fetch(event.request);
+    })
+  );
+});
